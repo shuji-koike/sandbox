6c123694fcae80646ff49c926cfe426642a920eb
Shuji Koike <shuji.koike@me.com>
2024-12-18 01:01:51 +0900

    fixup! [ci skip] WIP WIP WIP WIP WIP

--- estree/index.ts
+++ estree/index.ts
@@ -1,25 +1,34 @@
+import {
+  AST_NODE_TYPES,
+  parse,
+  simpleTraverse,
+} from "@typescript-eslint/typescript-estree"
 import { opendir, readFile, writeFile } from "node:fs/promises"
-import { parse } from "@typescript-eslint/typescript-estree"
 import path from "node:path"
 import ts from "typescript"
 import { readConfigFile } from "./utils"
 
-const baseDir = "/Users/shuji/github.com/terass-inc/tera-two"
-const config = readConfigFile(baseDir)
-
 console.debug = () => {}
 
 const files: Record<string, Awaited<ReturnType<typeof parseFile>> | null> = {}
-const deps: { from: string; to: string; source: string }[] = []
+const deps: { from: string; to?: string; source: string }[] = []
+
+main({})
 
 async function main({
+  baseDir,
   write = true,
   safe = false,
-}: { write?: boolean; safe?: boolean } = {}) {
+}: {
+  baseDir?: string
+  write?: boolean
+  safe?: boolean
+}) {
+  baseDir = path.resolve(process.env.BASE_DIR || ".")
   for await (const filePath of walk(baseDir)) {
     console.debug(filePath)
     try {
-      files[filePath] = await parseFile(filePath)
+      files[filePath] = await parseFile(baseDir, filePath)
     } catch (error) {
       if (safe) throw error
       console.error(error)
@@ -32,37 +41,54 @@ async function main({
   }
 }
 
-async function parseFile(filePath: string) {
+async function parseFile(baseDir: string, filePath: string) {
+  console.log(parseFile.name, filePath)
   const code = await readFile(filePath, "utf-8")
+  const config = readConfigFile(baseDir)
   const ast = parse(code, {
     filePath,
     jsx: true,
     // comment: true,
     loc: true,
     range: true,
-    tokens: true,
+    tokens: false,
   })
-  for (const e of ast.body) {
-    if (e.type === "ImportDeclaration") {
-      if (/\.(jpg|png|svg|css)/.test(e.source.value)) continue
-      console.debug(filePath, e.source.value)
-      const { resolvedModule } = ts.resolveModuleName(
-        e.source.value,
-        filePath,
-        config.options,
-        ts.sys,
-      )
-      if (!resolvedModule) {
-        console.error(filePath, e.source.value)
-        throw new Error("module not found")
+  simpleTraverse(ast, {
+    enter(node) {
+      if (node.type === AST_NODE_TYPES.ImportDeclaration) {
+        if (/\.(jpg|png|svg|css)/.test(node.source.value)) return
+        console.log(filePath, node.source.value)
+        resolve(node.source.value)
       }
-      console.debug(e.source.value, resolvedModule?.resolvedFileName)
-      deps.push({
-        from: filePath,
-        to: resolvedModule.resolvedFileName,
-        source: e.source.value,
-      })
+      if (node.type === AST_NODE_TYPES.ImportExpression) {
+        if (
+          node.source.type === AST_NODE_TYPES.Literal &&
+          typeof node.source.value === "string"
+        ) {
+          console.log(filePath, node.source.value)
+          resolve(node.source.value)
+        } else {
+          console.error("ImportExpression: source is not a Literal", filePath)
+        }
+      }
+    },
+  })
+  function resolve(source: string) {
+    const { resolvedModule } = ts.resolveModuleName(
+      source,
+      filePath,
+      config.options,
+      ts.sys,
+    )
+    if (!resolvedModule) {
+      console.error("module not found", filePath, source)
     }
+    console.debug(source, resolvedModule?.resolvedFileName)
+    deps.push({
+      from: filePath,
+      to: resolvedModule?.resolvedFileName,
+      source,
+    })
   }
   return ast
 }
@@ -80,5 +106,3 @@ async function* walk(dir: string): AsyncGenerator<string> {
     }
   }
 }
-
-main()
